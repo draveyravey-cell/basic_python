a5b44c48321d83bbf452736f09f4d51c1a384062
draveyravey-cell <draveyravey@gmail.com>
2026-02-25 22:26:09 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/basic_python/", // 👈 ESSENCIAL
+  // ❌ REMOVE o base
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
